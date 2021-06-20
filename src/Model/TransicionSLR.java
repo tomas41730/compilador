eac3d520599c50f4d5561d80679b5528b9f000ef
javivi235 <javisoruco2305@gmail.com>
2021-06-20 15:23:36 -0400

    COM-09: bug de siguientes arreglado y gramatica arreglada

--- src/Model/TransicionSLR.java
+++ src/Model/TransicionSLR.java
@@ -4,11 +4,13 @@ public class TransicionSLR {
 
     private int destino;
     private String input;
+    private String operacion;
 
     public TransicionSLR (int destino, String input) {
 
         this.destino = destino;
         this.input = input;
+
 
     }
 
